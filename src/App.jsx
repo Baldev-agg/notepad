ff462d49eff3a0d2992a557ade4907cd494f1647
Baldev Aggarwal <baldevdevlope1@gmail.comm>
2026-01-28 15:26:59 +0530

    notepad

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function App() {
+  const [text, setText] = useState("");
+  const [notes, setNotes] = useState([]);
+
+const API_BASE = import.meta.env.VITE_API_BASE_URL;
+
+  const fetchNotes = async () => {
+    const res = await axios.get(`${API_BASE}/api/notes`);
+    setNotes(res.data);
+  };
+
+  const saveNote = async () => {
+    await axios.post(`${API_BASE}/api/notes`, {
+      content: text,
+    });
+    setText("");
+    fetchNotes();
+  };
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  return (
+    <>
+      <h2>Notepad App</h2>
+      <textarea value={text} onChange={(e) => setText(e.target.value)} />
+      <br />
+      <button onClick={saveNote}>Save</button>
+
+      <ul>
+        {notes.map((n) => (
+          <li key={n.id}>{n.content}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default App;
